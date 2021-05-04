ad9312786ad1f9097279e7e79dce4b5e47ad46b0
varvara2510 <72070078+varvara2510@users.noreply.github.com>
2021-05-04 10:57:16 +0300

    Add files via upload
    загрузила основной код

---
+++ functions.py
@@ -0,0 +1,110 @@
+import random
+
+def pretty_print(l):#функция, которая принимает массив и проходя по всем рядам массива, печатает каждый вложенный список
+    print('-'*10) #чтобы отделять один вывод от другого
+    for row in l:
+        print(*row)
+    print('-' * 10)
+
+def get_number_from_index(i, j): #функция, которая принимает номер строки и номер столбцами возвращает порядковый номер элемента
+    return i * 4 + j + 1
+
+def get_index_from_number(num):  #функция, которая по числу выдает координаты ячейки
+    num -= 1
+    x,y = num // 4, num % 4
+    return x,y
+
+def insert_2_or_4(l,x,y):        #функция, вставляющая двойку или четверку в массив
+    if random.random()<=0.8:    #так как в оригинальной игре двойка выпадает чаще, чем четверка, увеличим вероятность выпадения двойки с 50% до 75%
+        l[x][y] = 2
+    else:
+        l[x][y] = 4
+    return l
+
+def get_empty_list(l): #функция для нахождения пустых мест в списке
+    empty = [] #список, в который записываются все порядковые номера пустых ячеек
+    for i in range(4):
+        for j in range(4):
+            if l[i][j] == 0:
+                num = get_number_from_index(i, j)
+                empty.append(num)
+    return empty
+
+def is_there_an_empty_cell(l):
+    for row in l:
+        if 0 in row:
+            return True
+    return False
+
+#заставляем ячейки двигаться
+def move_left(l):
+    for row in l:
+        while 0 in row:
+            row.remove(0)
+        while len(row) != 4:
+            row.append(0)
+    for i in range(4):
+        for j in range(3):
+            if l[i][j] == l[i][j + 1] and l[i][j] != 0:
+                l[i][j] *= 2
+                l[i].pop(j + 1)
+                l[i].append(0)
+    return l
+
+def move_right(l):
+    for row in l:
+        while 0 in row:
+            row.remove(0)
+        while len(row) != 4:
+            row.insert(0, 0) #вставляем нули вначало, потому что ответ должен складываться направо
+    for i in range(4):
+        for j in range(3, 0, -1):
+            if l[i][j] == l[i][j - 1] and l[i][j] != 0:
+                l[i][j] *= 2
+                l[i].pop(j - 1)
+                l[i].insert(0, 0)
+    return l
+
+def move_up(l):
+    for j in range(4):
+        column = []
+        for i in range(4):
+            if l[i][j] != 0:
+                column.append(l[i][j])
+        while len(column) != 4:
+            column.append(0)
+        for i in range(3):
+            if column[i] == column[i + 1] and column[i] != 0:
+                column[i] *= 2
+                column.pop(i + 1)
+                column.append(0)
+        for i in range(4):
+            l[i][j] = column[i]
+    return l
+
+def move_down(l):
+    for j in range(4):
+        column = []
+        for i in range(4):
+            if l[i][j] != 0:
+                column.append(l[i][j])
+        while len(column) != 4:
+            column.insert(0, 0)
+        for i in range(3, 0, -1):
+            if column[i] == column[i - 1] and column[i] != 0:
+                column[i] *= 2
+                column.pop(i - 1)
+                column.insert(0, 0)
+        for i in range(4):
+            l[i][j] = column[i]
+    return l
+
+def is_there_a_way_out(l):
+    for i in range(3):
+        for j in range(3):
+            if l[i][j] == l[i][j + 1] or l[i][j] == l[i + 1][j]:
+                return True
+    return False
+
+
+
---
+++ tests.py
@@ -0,0 +1,165 @@
+import unittest
+from functions import get_number_from_index, get_empty_list, get_index_from_number, is_there_an_empty_cell, \
+move_left, move_up, move_down, is_there_a_way_out
+
+class Test_2048(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(get_number_from_index(1,2),7)
+
+    def test_2(self):
+        self.assertEqual(get_number_from_index(3,3),16)
+
+    def test_3(self):
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        l = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(get_empty_list(l),a)
+
+    def test_4(self):
+        a = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        l = [
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(get_empty_list(l), a)
+
+    def test_5(self):
+        a = []
+        l = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+        self.assertEqual(get_empty_list(l), a)
+
+    def test_6(self):
+        self.assertEqual(get_index_from_number(7),(1,2))
+
+    def test_7(self):
+        self.assertEqual(get_index_from_number(16),(3,3))
+
+    def test_8(self):
+        self.assertEqual(get_index_from_number(1),(0,0))
+
+    def test_9(self):
+        a = []
+        l = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+        self.assertEqual(is_there_an_empty_cell(l), False)
+
+    def test_10(self):
+        a = []
+        l = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [0, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+        self.assertEqual(is_there_an_empty_cell(l), True)
+
+    def test_11(self):
+        a = []
+        l = [
+            [1, 1, 1, 0],
+            [0, 1, 1, 0],
+            [1, 1, 1, 1],
+            [0, 1, 1, 1],
+        ]
+        self.assertEqual(is_there_an_empty_cell(l), True)
+    def test_12(self):
+        a = []
+        l = [
+            [2, 2, 0, 0],
+            [0, 4, 4, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        rez = [
+            [4, 0, 0, 0],
+            [8, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+
+        ]
+        self.assertEqual(move_left(l), rez)
+    def test_13(self):
+        a = []
+        l = [
+            [2, 4, 4, 2],
+            [4, 0, 0, 2],
+            [0, 0, 0, 0],
+            [8, 8, 4, 4],
+        ]
+        rez = [
+            [2, 8, 2, 0],
+            [4, 2, 0, 0],
+            [0, 0, 0, 0],
+            [16, 8, 0, 0],
+
+        ]
+        self.assertEqual(move_left(l), rez)
+    def test_14(self):
+        a = []
+        l = [
+            [2, 4, 0, 2],
+            [2, 0, 2, 0],
+            [4, 0, 2, 4],
+            [4, 4, 0, 0],
+        ]
+        rez = [
+            [4, 8, 4, 2],
+            [8, 0, 0, 4],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+
+        ]
+        self.assertEqual(move_up(l), rez)
+
+    def test_15(self):
+        a = []
+        l = [
+            [2, 4, 0, 2],
+            [2, 0, 2, 0],
+            [4, 0, 2, 4],
+            [4, 4, 0, 0],
+        ]
+        rez = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [4, 0, 0, 2],
+            [8, 8, 4, 4],
+
+        ]
+        self.assertEqual(move_down(l), rez)
+
+    def test_16(self):
+        a = []
+        l = [
+            [2, 4, 0, 2],
+            [2, 0, 2, 0],
+            [4, 0, 2, 4],
+            [4, 4, 0, 0],
+        ]
+        self.assertEqual(is_there_a_way_out(l), True)
+
+    def test_17(self):
+        a = []
+        l = [
+            [2, 4, 8, 2],
+            [11, 16, 5, 2],
+            [9, 31, 2, 4],
+            [4, 74, 34, 56],
+        ]
+        self.assertEqual(is_there_a_way_out(l), False)
